49356906a652f22c5efb46d5dd495a95b4eeeeed
Josue <josuepereztorr@gmail.com>
2023-04-14 23:56:46 -0500

    completed app

---
+++ module2labb/module2labb/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace module2labb
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DateSelected(object sender, DateChangedEventArgs e)
+        {
+            int today = DateTime.Now.Year;
+            int userDOB = e.NewDate.Year;
+
+            int numOfYearsUntil21 = 21 - (today - userDOB);
+
+            if (numOfYearsUntil21 > 0)
+            {
+                NumOfYears.Text = numOfYearsUntil21.ToString();
+            }
+            else
+            {
+                NumOfYears.Text = "0";
+            }
+
+            
+        }
+    }
+}
+
